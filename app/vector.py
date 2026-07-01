79349fef77cf72fef261d9c68d2247272ede7693
atig issra <issratig1@gmail.com>
2026-07-01 12:45:13 +0100

    working on vector.py

--- app/vector.py
+++ app/vector.py
@@ -16,7 +16,12 @@ print(f"🔗 vector.py -> Connexion à Ollama sur : {OLLAMA_BASE_URL}")
 # 2. DATA LOADING & PREPARATION
 # ==========================================
 print("📖 Lecture du CSV...")
-df = pd.read_csv("/data/medquad_cvd_final.csv")
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+csv_path = os.path.join(BASE_DIR, "data", "medquad_cvd_final.csv")
+
+df = pd.read_csv(csv_path)
 print(f"✅ CSV chargé : {len(df)} lignes")
 
 print("📝 Création des documents...")
